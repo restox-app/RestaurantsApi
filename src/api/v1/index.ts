aaf232ab415ae4bf9e4dd409b8973668627633a4
Vinayak Nivalkar <57988280+VinayakNivalkar@users.noreply.github.com>
2023-06-11 12:46:29 +0530

    last commit

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -1,5 +1,33 @@
 import { FastifyPluginAsync } from 'fastify';
 
-export const v1: FastifyPluginAsync = async (instance, _opts) => {
+import * as index from './routes';
 
+export const v1: FastifyPluginAsync = async (instance, _opts) => {
+  instance.route({
+    url: '/',
+    method: 'GET',
+    schema: index.GET_validation_schema,
+    handler: index.GET_handler,
+  });
+
+  instance.route({
+    url: '/:restaurant_id',
+    method: 'GET',
+    schema: index.singular.GET_validation_schema,
+    handler: index.singular.GET_handler,
+  });
+
+  instance.route({
+    url: '/:restaurant_id/menu',
+    method: 'GET',
+    schema: index.singular.menu.GET_validation_schema,
+    handler: index.singular.menu.GET_handler,
+  });
+
+  instance.route({
+    url: '/:restaurant_id/menu/:menu_item_id',
+    method: 'GET',
+    schema: index.singular.menu.singular.GET_validation_schema,
+    handler: index.singular.menu.singular.GET_handler,
+  });
 };
---
+++ src/api/v1/routes/index.ts
@@ -0,0 +1,2 @@
+export * from './GET';
+export * as singular from './singular';
